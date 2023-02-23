5c52b5a8a3130ab6051b36c218b4954440944821
Kr1jn <113419502+Kr1jn@users.noreply.github.com>
2023-02-23 11:44:52 +0100

    fix

--- Jaar_1/Module_6/challange_4/script.js
+++ Jaar_1/Module_6/challange_4/script.js
@@ -1,11 +1,32 @@
+let matrix = [
+  [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0, 2, 2, 0, 0, 0],
+];
 const container = document.getElementById("container");
 //de variablen
 let id = 0;
+let row = 0;
+let colum = 0;
 //de for loop die alle squares genereed en de squares een identiek ID geeft
-for (let x = 0; x < 2000; x++) {
-  id++;
-  const div = document.createElement("div");
-  div.className = "square";
-  div.id = "square_" + id;
-  container.appendChild(div);
+// for (let x = 0; x < 2000; x++) {
+//   id++;
+//   const div = document.createElement("div");
+//   div.className = "squareijbae"+ matrix[row][colum];
+//   div.id = "square_" + id;
+//   container.appendChild(div);
+//   colum++;
+//   if (colum > 10) {
+//     colum = 0;
+//     row++;
+//   };
+// }
+
+for (let y = 0; y < matrix.length; y++) {
+  for (let x = 0; x < matrix[y].length; x++) {
+    console.log("matrix " + matrix[y][x]);
+    const div = document.createElement("div");
+    div.className = "squareijbae" + matrix[y][x];
+    div.id = "square_" + id;
+    container.appendChild(div);
+  }
 }
