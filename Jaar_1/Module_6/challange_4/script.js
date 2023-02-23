aeea1d9e55cddc05d6c90f83ae86fdfaa0a243fe
Kr1jn <113419502+Kr1jn@users.noreply.github.com>
2023-02-23 11:06:19 +0100

    fix7

--- Jaar_1/Module_6/challange_4/script.js
+++ Jaar_1/Module_6/challange_4/script.js
@@ -1,7 +1,11 @@
 const container = document.getElementById("container");
-
-for(let x=0; x<2000; x++) {
-    const div = document.createElement("div");
-    div.className = "vakje";
-    container.appendChild(div);
+//de variablen
+let id = 0;
+//de for loop die alle squares genereed en de squares een identiek ID geeft
+for (let x = 0; x < 2000; x++) {
+  id++;
+  const div = document.createElement("div");
+  div.className = "square";
+  div.id = "square_" + id;
+  container.appendChild(div);
 }
